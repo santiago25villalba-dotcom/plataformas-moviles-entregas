463cb507684a4dec51ffc8d85cfe476415ef6778
santiago25villalba-dotcom <santiago25villalba@gmail.com>
2026-09-01 21:30:39 -0300

    create listas.js en tp-9

--- tp-9/listas.js
+++ tp-9/listas.js
@@ -1,1 +1,131 @@
+   var listaNumerosEjemplo = [2, 4, 6, -1, 5, -4, 0];
 
+/**
+ * 01 - invertirLista
+ */
+function invertirLista(listaDeNumeros) {
+    return listaDeNumeros.slice().reverse();
+}
+console.log("invertirLista([2,3,4]): ", invertirLista([2, 3, 4]));
+
+/**
+ * 02 - sumarLista
+ */
+function sumarLista(listaDeNumeros) {
+    return listaDeNumeros.reduce((acc, curr) => acc + curr, 0);
+}
+console.log("sumarLista([2,3,4]): ", sumarLista([2, 3, 4]));
+
+/**
+ * 03 - contarElementosLista
+ */
+function contarElementosLista(listaDeNumeros) {
+    return listaDeNumeros.length;
+}
+console.log("contarElementosLista([2,3,4]): ", contarElementosLista([2, 3, 4]));
+
+/**
+ * 04 - calcularPromedio
+ */
+function calcularPromedio(listaDeNumeros) {
+    if (listaDeNumeros.length === 0) return 0;
+    return sumarLista(listaDeNumeros) / contarElementosLista(listaDeNumeros);
+}
+console.log("calcularPromedio([2,3,4]): ", calcularPromedio([2, 3, 4]));
+console.log("calcularPromedio(listaNumerosEjemplo): ", calcularPromedio(listaNumerosEjemplo));
+
+/**
+ * 05 - triplicarLista
+ */
+function triplicarLista(listaDeNumeros) {
+    return listaDeNumeros.map(n => n * 3);
+}
+console.log("triplicarLista([1, 2, 3]): ", triplicarLista([1, 2, 3]));
+
+/**
+ * 06 - crearListaDeNumeros
+ */
+function crearListaDeNumeros(inicio, fin) {
+    var resultado = [];
+    for (var i = inicio; i <= fin; i++) {
+        resultado.push(i);
+    }
+    return resultado;
+}
+console.log("crearListaDeNumeros(2,5): ", crearListaDeNumeros(2, 5));
+
+/**
+ * 07 - ordenarDeMayorAMenor
+ */
+function ordenarDeMayorAMenor(listaDeNumeros) {
+    return listaDeNumeros.slice().sort((a, b) => b - a);
+}
+console.log("ordenarDeMayorAMenor([2,3,4]): ", ordenarDeMayorAMenor([2, 3, 4]));
+console.log("ordenarDeMayorAMenor(listaNumerosEjemplo): ", ordenarDeMayorAMenor(listaNumerosEjemplo));
+
+/**
+ * 08 - encontrarNumeroMayor
+ */
+function encontrarNumeroMayor(listaDeNumeros) {
+    return Math.max(...listaDeNumeros);
+}
+console.log("encontrarNumeroMayor([2,3,4]): ", encontrarNumeroMayor([2, 3, 4]));
+console.log("encontrarNumeroMayor(listaNumerosEjemplo): ", encontrarNumeroMayor(listaNumerosEjemplo));
+
+/**
+ * 09 - ordenarPalabrasPorLongitud
+ */
+function ordenarPalabrasPorLongitud(listaDePalabras) {
+    return listaDePalabras.slice().sort((a, b) => a.length - b.length);
+}
+console.log("ordenarPalabrasPorLongitud(['abc', 'a', 'ab']): ", ordenarPalabrasPorLongitud(['abc', 'a', 'ab']));
+
+/**
+ * 10 - encontrarPalabraMasCorta
+ */
+function encontrarPalabraMasCorta(listaDePalabras) {
+    return ordenarPalabrasPorLongitud(listaDePalabras)[0];
+}
+console.log("encontrarPalabraMasCorta(['abc', 'a', 'ab']): ", encontrarPalabraMasCorta(['abc', 'a', 'ab']));
+
+/**
+ * 11 - filtrarSoloPositivos
+ */
+function filtrarSoloPositivos(listaDeNumeros) {
+    return listaDeNumeros.filter(n => n > 0);
+}
+console.log("filtrarSoloPositivos([1,-1,0]): ", filtrarSoloPositivos([1, -1, 0]));
+console.log("filtrarSoloPositivos(listaNumerosEjemplo): ", filtrarSoloPositivos(listaNumerosEjemplo));
+
+/**
+ * 12 - contarAprobados
+ */
+function contarAprobados(listaDeNotas) {
+    return listaDeNotas.filter(n => n >= 6).length;
+}
+console.log("contarAprobados([10, 4, 6, 7, 1, 9]): ", contarAprobados([10, 4, 6, 7, 1, 9]));
+
+/**
+ * 13 - filtrarSoloTruthy
+ */
+function filtrarSoloTruthy(listaDeValores) {
+    return listaDeValores.filter(Boolean);
+}
+console.log("filtrarSoloTruthy(): ", filtrarSoloTruthy(["Hola", "", null, 1, 0, -1, undefined, [], {}]));
+
+/**
+ * 14 - enumerarLista
+ */
+function enumerarLista(listaDePalabras) {
+    if (listaDePalabras.length === 0) return "";
+    if (listaDePalabras.length === 1) return `${listaDePalabras[0]}.`;
+    
+    var ultimasDos = listaDePalabras.slice(-2).join(" y ");
+    var primeras = listaDePalabras.slice(0, -2);
+    
+    if (primeras.length > 0) {
+        return `${primeras.join(", ")}, ${ultimasDos}.`;
+    }
+    return `${ultimasDos}.`;
+}
+console.log("enumerarLista(): ", enumerarLista(["Han", "Leia", "Luke", "Yoda"]));
